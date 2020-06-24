7b8385efe01897d77916560cda6c055ffa31a1fe
itea.dev <itea.dev@outlook.com>
2020-06-24 13:35:35 +0200

    Display List of Products

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  products = [];
 
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.apiService.get().subscribe((data: any[]) => {
+      console.log(data);
+      this.products = data;
+    });
   }
-
 }
